d3dc0a667c4154de533ff99597074c9c73af1205
Anastasiya Zenevich <a.zenevich@bk.ru>
2026-03-12 22:38:48 +0300

    add changes

--- Lesson_6_HT.js
+++ Lesson_6_HT.js
@@ -97,9 +97,9 @@
 // //     {name: "dog", legs: 4, color: "yellow"}
 // //     Возвратите строку: "This yellow dog has 4 legs."
 // const dogDescription = {
-//     name: "dog", 
-//     legs: 4, 
-//     color: "yellow" 
+//     name: "dog",
+//     legs: 4,
+//     color: "yellow"
 // };
 
 // function returnDogDescription(object) {
@@ -149,46 +149,271 @@
 
 // 6. Есть список людей, которые подписались на международную встречу разработчиков.
 
- var developers = [
-     { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', age: 29, language: 'Ruby' },
-     { firstName: 'Peter', lastName: 'B.', country: 'Poland', age: 48, language: 'Javascript' },
-     { firstName: 'Natasha', lastName: 'P.', country: 'Mexico', age: 25, language: 'C#' },
-     { firstName: 'Josh', lastName: 'A.', country: 'USA', age: 26, language: 'C#' },
-     { firstName: 'Augusto', lastName: 'C.', country: 'Spain', age: 32, language: 'Java' },
-     { firstName: 'Lei Mi', lastName: 'S.', country: 'China', age: 52, language: 'Fortran' },
-     { firstName: 'Mikey', lastName: 'L.', country: 'New Zealand', age: 30, language: 'Node' }
- ];
+var developers = [
+  {
+    firstName: "Emma",
+    lastName: "Z.",
+    country: "Netherlands",
+    age: 29,
+    language: "Ruby",
+  },
+  {
+    firstName: "Peter",
+    lastName: "B.",
+    country: "Poland",
+    age: 48,
+    language: "Javascript",
+  },
+  {
+    firstName: "Natasha",
+    lastName: "P.",
+    country: "Mexico",
+    age: 25,
+    language: "C#",
+  },
+  {
+    firstName: "Josh",
+    lastName: "A.",
+    country: "USA",
+    age: 26,
+    language: "C#",
+  },
+  {
+    firstName: "Augusto",
+    lastName: "C.",
+    country: "Spain",
+    age: 32,
+    language: "Java",
+  },
+  {
+    firstName: "Lei Mi",
+    lastName: "S.",
+    country: "China",
+    age: 52,
+    language: "Fortran",
+  },
+  {
+    firstName: "Mikey",
+    lastName: "L.",
+    country: "New Zealand",
+    age: 30,
+    language: "Node",
+  },
+];
 
 //     Вопрос: есть ли хоть один разработчик, работающий на JavaScript? Возвратите да или нет. Будьте внимательны.
 
-let javaScriptDevelopersList = developers.filter(user => user.language.toLowerCase() === 'JavaScript'.toLowerCase());
-let count = javaScriptDevelopersList.length > 0
+let javaScriptDevelopersList = developers.filter(
+  (user) => user.language.toLowerCase() === "JavaScript".toLowerCase(),
+);
+let count = javaScriptDevelopersList.length > 0;
 console.log(count);
 
 // 7. Задача для сладкоежек. Есть массив десертов. Выведите самый дешевый десерт и самый дорогой десерт.
 
-    const desserts = [
-        { name: "Пирожное", price: 65 },
-        { name: "Мороженое", price: 35 },
-        { name: "Торт Наполеон", price: 250 },
-        { name: "Песочное Печенье", price: 50 },
-        { name: "Пудинг", price: 80 },
-        { name: "Фруктовый Тарт", price: 40 },
-        { name: "Желе Земляничное", price: 40 },
-        { name: "Вафли Шоколадные", price: 36 },
-        { name: "Булочка с Изюмом", price: 28 }
-    ];
+const desserts = [
+  { name: "Пирожное", price: 65 },
+  { name: "Мороженое", price: 35 },
+  { name: "Торт Наполеон", price: 250 },
+  { name: "Песочное Печенье", price: 50 },
+  { name: "Пудинг", price: 80 },
+  { name: "Фруктовый Тарт", price: 40 },
+  { name: "Желе Земляничное", price: 40 },
+  { name: "Вафли Шоколадные", price: 36 },
+  { name: "Булочка c Изюмом", price: 28 },
+];
+
+let minPriceDes = desserts.reduce((previous, current) => {
+  return previous.price < current.price ? previous : current;
+});
+console.log(minPriceDes);
+
+let maxPriceDes = desserts.reduce((previous, current) => {
+  return previous.price > current.price ? previous : current;
+});
+console.log(maxPriceDes);
 
 // 8. Креативное задание.
 
-// Создайте библиотеку книг Стивена Кинга, Марка Твена и Александра Сергеевича. В библиотеке должно быть по 5 книг каждого писателя. Каждая книга должна иметь идентификатор (isbn), имя книги, имя автора, год выпуска, жанр, количество страниц, популярность книги у этого автора (от 1 до 5) и
-// первое предложение/строку.
+// Создайте библиотеку книг Стивена Кинга, Марка Твена и Александра Сергеевича. В библиотеке должно быть по 5 книг каждого писателя. Каждая
+// книга должна иметь идентификатор (isbn), имя книги, имя автора, год выпуска, жанр, количество страниц, популярность книги у этого
+// автора (от 1 до 5) и первое предложение/строку.
+
+const library = [
+  {
+    author: "Стивен Кинг",
+    isbn: "978-0-385-08695-0",
+    bookname: "Carrie",
+    genre: "horror",
+    year: 1974,
+    pages: 199,
+    popularity: 5,
+    "first sentense":
+      "Carrie is the debut horror novel by American author Stephen King, released in 1974.",
+  },
+  {
+    author: "Стивен Кинг",
+    isbn: "978-0-385-00751-1",
+    bookname: "Salem's Lot",
+    genre: "horror",
+    year: 1975,
+    pages: 439,
+    popularity: 4,
+    "first sentense":
+      "Salem's Lot is a 1975 American horror novel by author Stephen King.",
+  },
+  {
+    author: "Стивен Кинг",
+    isbn: "978-0-385-12167-5",
+    bookname: "The Shining",
+    genre: "horror ",
+    year: 1977,
+    pages: 447,
+    popularity: 3,
+    "first sentense":
+      "The Shining is a 1977 horror novel by American author Stephen King.",
+  },
+  {
+    author: "Стивен Кинг",
+    isbn: "978-0-385-12168-2",
+    bookname: "The Stand",
+    genre: ["Post-apocalyptic", "Dark fantasy"],
+    year: 1978,
+    pages: 823,
+    popularity: 5,
+    "first sentense":
+      "The Stand is an epic post-apocalyptic dark fantasy novel written by American author Stephen King and first published in 1978 by Doubleday.",
+  },
+  {
+    author: "Стивен Кинг",
+    isbn: "978-0-670-85503-2",
+    bookname: "Insomnia",
+    genre: ["horror", "fantasy"],
+    year: 1994,
+    pages: 787,
+    popularity: 4,
+    "first sentense":
+      "Insomnia is a 1994 horror/fantasy novel by American writer Stephen King. ",
+  },
+  {
+    isbn: "0486280616",
+    bookname: "Adventures of Huckleberry Finn",
+    author: "Mark Twain",
+    year: 1876,
+    genre: "novel",
+    "number of pages": 224,
+    popularity: 5,
+    "first sentense":
+      'The novel\'s preeminence derives from its wonderfully imaginative re-creation of boyhood adventures along the Mississippi River, its inspired characterization, the author\'s remarkable ear for dialogue, and the book\'s understated development of serious underlying themes: "natural" man versus "civilized" society, the evils of slavery, the innate value and dignity of human beings, and other topics.',
+  },
+  {
+    isbn: "0375756817",
+    bookname: "The Adventures of Tom Sawyer",
+    author: "Mark Twain",
+    year: 2001,
+    genre: "novels",
+    "number of pages": 304,
+    popularity: 4,
+    "first sentense":
+      "This irresistible tale of the adventures of two friends growing up in frontier America is one of Mark Twain's most popular novels.",
+  },
+  {
+    isbn: "0140430644",
+    bookname: "A Connecticut Yankee in King Arthur's Court",
+    author: "Mark Twain",
+    year: 1972,
+    genre: "classic",
+    "number of pages": 416,
+    popularity: 4,
+    "first sentense":
+      "Mark Twain moves from broad comedy to biting social satire in this literary classic.",
+  },
+  {
+    isbn: "1789430976",
+    bookname: "Tom Sawyer & Huckleberry Finn",
+    author: "Mark Twain",
+    year: 2020,
+    genre: "adventure",
+    "number of pages": 592,
+    popularity: 4,
+    "first sentense":
+      "Mark Twain's characters are surprising, unforgettable and truly human.",
+  },
+  {
+    isbn: "0866119655",
+    bookname: "The Adventures of Huckleberry Finn",
+    author: "Mark Twain",
+    year: 1990,
+    genre: "adventure",
+    "number of pages": 239,
+    popularity: 4,
+    "first sentense":
+      'Climb aboard the raft with Huck and Jim and drift away from the "sivilized" life and into a world of adventure, excitement, danger, and self-discovery.',
+  },
+  {
+    isbn: "978-5-00155-691-6",
+    bookname: "Eugene Onegin",
+    author: "Aleksander Sergeevich",
+    year: 1833,
+    genre: "novel",
+    "number of pages": 288,
+    popularity: 3,
+    "first sentense":
+      "The story is told by a narrator (a lightly fictionalized version of Pushkin's public image), whose tone is educated, worldly, and intimate.",
+  },
+  {
+    isbn: "978-5-04-207769-2",
+    bookname: "The Captain's Daughter",
+    author: "Aleksander Sergeevich",
+    year: 1836,
+    genre: "novel",
+    "number of pages": 192,
+    popularity: 4,
+    "first sentense":
+      "A classic Russian historical military novel set during the Pugachev Rebellion which contrasts the human internal world with the inevitable movements of history.",
+  },
+  {
+    isbn: "978-5-04-102519-9",
+    bookname: "Boris Godunov",
+    author: "Aleksander Sergeevich",
+    year: 1825,
+    genre: "drama",
+    "number of pages": 256,
+    popularity: 4,
+    "first sentense":
+      "Alexander Pushkin's dramatic work displays a scintillating variety of forms, from the historical to the metaphysical and folkloric. After Boris Godunov, they evolved into Pushkin's own unique, condensed transformations of Western European themes and traditions.",
+  },
+  {
+    isbn: "978-5-699-47073-0",
+    bookname: "Pikovaya dama",
+    author: "Aleksander Sergeevich",
+    year: 1834,
+    genre: "Short stories",
+    "number of pages": 74,
+    popularity: 3,
+    "first sentense":
+      "The Queen of Spades (Russian: «Пиковая дама», romanized: Pikovaya dama) is an 1834 novella with supernatural elements by Alexander Pushkin, about human avarice.",
+  },
+  {
+    isbn: "978-5-04-172862-5",
+    bookname: "Ruslan and Lyudmila",
+    author: "Aleksander Sergeevich",
+    year: 1820,
+    genre: "poem",
+    "number of pages": 240,
+    popularity: 5,
+    "first sentense":
+      "In order to rescue his beloved Lyudmila, who has been abducted by the evil wizard Chernomor, the warrior Ruslan faces an epic and perilous quest, encountering a multitude of fantastic and terrifying characters along the way.",
+  },
+];
 
 //     a. Выведите в console названия всех книг.
+
+let booksName = library.map(element => element.bookname)
+console.log(booksName);
+
 //     b. Выведите в console имена самой популярной книги каждого из авторов в читабельном формате
 //     c. Отсортируйте библиотеку по году выпуска книг
-
-
 
 // 1. У вас есть массив чисел, нужно наполнить массив числами первого массива, умноженными на 2.
 // const nums = [3, 88, 65, 5, 9]; через мап
